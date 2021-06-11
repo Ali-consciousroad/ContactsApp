59184af1a7c1ec2726f98c7c89778b6a2116bd5c
Ali-consciousroad <ali_dindar@live.be>
2021-06-11 20:36:07 +0200

    Add ContactList component with specific props for each created component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+
+class ContactList extends React.Component {
+  render(){
+    // All components will have the same names 
+    /*
+    const people = [
+      { name: 'Tyler'},
+      { name: 'Karen'},
+      { name: 'Richard'}
+    ]*/
+
+    // Use React properties to allow specific properties for each component
+    const people = this.props.contacts 
+
+    // .map() won't work if we don't have values for each created component
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContactList contacts={[
+        { name: 'Hassan '},
+        { name: 'Hussein '},
+        { name: 'Ali '}
+      ]} />
+        <ContactList contacts={[
+        { name: 'Jean '},
+        { name: 'Pierre '},
+        { name: 'Arthur '}
+      ]}  />
+        <ContactList contacts={[
+        { name: 'Obama '},
+        { name: 'Bolt '},
+        { name: 'Jordan '}
+      ]}  />
     </div>
   );
 }
