9ac13f7691a8b04ec2166aaf44aa5f5c225e5768
Ali-consciousroad <ali_dindar@live.be>
2021-12-16 13:44:06 +0100

    Display contact avatar, name and login

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
-import './App.css';
 
 const contacts = [
   {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
+    id: "karen",
+    name: "Karen Isgrigg",
+    handle: "karen_isgrigg",
+    avatarURL: "http://localhost:5001/karen.jpg"
   },
   {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
+    id: "richard",
+    name: "Richard Kalehoff",
+    handle: "richardkalehoff",
+    avatarURL: "http://localhost:5001/richard.jpg"
   },
   {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
+    id: "tyler",
+    name: "Tyler McGinnis",
+    handle: "tylermcginnis",
+    avatarURL: "http://localhost:5001/tyler.jpg"
   }
  ];
  
 class ContactList extends React.Component {
   render(){
-    // All components will have the same names 
-    /*
-    const people = [
-      { name: 'Tyler'},
-      { name: 'Karen'},
-      { name: 'Richard'}
-    ]*/
-
     // Use React properties to allow specific properties for each component
     const people = this.props.contacts 
-
     // .map() won't work if we don't have values for each created component
     return <ol>
       {people.map((person) => (
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react'
 
 class ListContacts extends Component {
-    render() {
-        //console.log('Props', this.props)
-        return (
-            <ol className='contact-list'>
-                {this.props.contacts.map((contact) => (
-                    //React use key to understand what has been changed without recreating the entire list
-                    <li key={contact.id}>
-                        {contact.name}
-                    </li>
-                ))}
-            </ol>
-        )
-    }
+  render() {
+    return (
+      <ol className='contact-list'>
+        {this.props.contacts.map((contact) => (
+          <li key={contact.id} className='contact-list-item'>
+            <div
+              className='contact-avatar'
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}
+            ></div>
+            <div className='contact-details'>
+              <p>{contact.name}</p>
+              <p>{contact.handle}</p>
+            </div>
+            <button className='contact-remove'>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+    )
+  }
 }
 
 export default ListContacts
