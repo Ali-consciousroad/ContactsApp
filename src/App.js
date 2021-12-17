669420fd4a96fedda403aecec9498dbc0550580f
Ali-consciousroad <ali_dindar@live.be>
2021-12-17 18:06:55 +0100

    Add the array inside our state as an object property to our state and adapt the code to the React Nanodegree tutorial

--- src/App.js
+++ src/App.js
@@ -1,46 +1,38 @@
-import React, { Component } from 'react';
-import ListContacts from './ListContacts';
+import React, { Component } from "react";
+import ListContacts from "./ListContacts";
 
-const contacts = [
-  {
-    id: "karen",
-    name: "Karen Isgrigg",
-    handle: "karen_isgrigg",
-    avatarURL: "http://localhost:5001/karen.jpg"
-  },
-  {
-    id: "richard",
-    name: "Richard Kalehoff",
-    handle: "richardkalehoff",
-    avatarURL: "http://localhost:5001/richard.jpg"
-  },
-  {
-    id: "tyler",
-    name: "Tyler McGinnis",
-    handle: "tylermcginnis",
-    avatarURL: "http://localhost:5001/tyler.jpg"
-  }
- ];
- 
-class ContactList extends React.Component {
-  render(){
-    // Use React properties to allow specific properties for each component
-    const people = this.props.contacts 
-    // .map() won't work if we don't have values for each created component
-    return <ol>
-      {people.map((person) => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
-}
+class App extends Component {
+  state = {
+    // Add a contact property to our state object
+    contacts: [
+      {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "karen_isgrigg",
+        avatarURL: "http://localhost:5001/karen.jpg",
+      },
+      {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "richardkalehoff",
+        avatarURL: "http://localhost:5001/richard.jpg",
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "tylermcginnis",
+        avatarURL: "http://localhost:5001/tyler.jpg",
+      },
+    ],
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <ListContacts contacts={contacts} />
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <ListContacts contacts={this.state.contacts} />
+      </div>
+    );
+  }
 }
 
 export default App;
